d13d9ec3da7d4b92fec72d2f2fc220ca39ee2d38
RUSLAN <138212155+Blountyyy@users.noreply.github.com>
2026-09-08 21:55:14 +0500

    Add files via upload
    Башмаков Руслан 1 домашнее задание

---
+++ Homework 1/src/Variables5.java
@@ -0,0 +1,13 @@
+void main() {
+    int number = 42;
+    double price = 19.99;
+    boolean isStudent = true;
+    String city = "Караганда";
+    char grade = 'A';
+
+    System.out.println(number);
+    System.out.println(price);
+    System.out.println(isStudent);
+    System.out.println(city);
+    System.out.println(grade);
+}
---
+++ Homework 1/src/Vizitka.java
@@ -0,0 +1,9 @@
+void main() {
+    String name = "Башмаков Руслан Максимович";
+    int age = 25;
+    String city = "Караганда";
+
+    System.out.println("Имя: " + name);
+    System.out.println("Возраст: " + age);
+    System.out.println("Город: " + city);
+}
